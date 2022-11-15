ca9644a74fcce14b2a6c67d41672ab5806f26196
GMcMichael <gmcmichael0717@gmail.com>
2022-11-15 14:41:43 -0800

    Updated graphs to take data from controller

---
+++ Habit Tracker - Doveloop/Models/Graph.cs
@@ -0,0 +1,27 @@
+﻿namespace Habit_Tracker___Doveloop.Models
+{
+    public struct GraphData
+    {
+        public int value;
+        public string label;
+        public string color;
+        public GraphData(int value, string label, string color)
+        {
+            this.value = value;
+            this.label = label;
+            this.color = color; 
+        }
+    }
+
+    public class Graph
+    {
+        public GraphData[] planned { get; set; }
+        public GraphData[] actual { get; set; }
+
+        public Graph(GraphData[] planned, GraphData[] actual)
+        {
+            this.planned = planned;
+            this.actual = actual;
+        }
+    }
+}
