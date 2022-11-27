e02d3839791cfe55a8e3c6409c289b7b86c621ec
GMcMichael <gmcmichael0717@gmail.com>
2022-11-26 16:17:42 -0800

    Fixed graph viewmodel input

--- Habit Tracker - Doveloop/Controllers/GraphController.cs
+++ Habit Tracker - Doveloop/Controllers/GraphController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Habit_Tracker___Doveloop.Models;
+using Habit_Tracker___Doveloop.Models.ViewModels;
 using Habit_Tracker___Doveloop.Data;
 
 namespace Habit_Tracker___Doveloop.Controllers
@@ -11,9 +12,24 @@ namespace Habit_Tracker___Doveloop.Controllers
         {
             _cosmosDbService = cosmosDbService;
         }
-        public IActionResult Index()
+
+        private HabitViewModel CreateHabitViewModel(HabitLabel habit, IEnumerable<HabitLabel> habitsLabels)
         {
-            return View(_cosmosDbService.GetHabitsLabelsAsync());
+            List<HabitLabel> labels = new List<HabitLabel>();
+            if (habit.RelationIds != null) habit.RelationIds.ForEach(id => {
+                HabitLabel label = habitsLabels.SingleOrDefault(l => l.Id == id);
+                if (label != null)
+                    labels.Add(label);
+            });
+            return new HabitViewModel(habit, labels);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<HabitViewModel> viewModels = new List<HabitViewModel>();
+            IEnumerable<HabitLabel> habitsLabels = await _cosmosDbService.GetHabitsLabelsAsync();
+            habitsLabels.Where(h => h.Type == "habit").ToList().ForEach(h => viewModels.Add(CreateHabitViewModel(h, habitsLabels)));
+            return View(viewModels);
             /*return View(new Graph(
                 new GraphData[]
                 {
